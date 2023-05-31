fd4666cd92e827a8216ac7b2b1c86be86258fd71
c.misman <mismancelaleddin@gmail.com>
2023-05-31 14:06:30 +0300

    jms added

---
+++ src/main/java/com/sodaware/web/config/JMSConfig.java
@@ -0,0 +1,20 @@
+package com.sodaware.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
